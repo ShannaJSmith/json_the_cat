e3b4c922224283945bb1a2d7963a536f02d5c8e6
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-23 20:23:38 +0000

    eslint

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,20 +3,20 @@ const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
 const fetchBreedDescription = function(breedName, callback) {
   request(`${url}${breedName}`, (error, response, body) => {
-  if (body === '[]') {
-    callback('ERROR: Breed not found', null);
-    return;
-  }
-  if (error) {   
-    callback(error, null);
-    return;
-  }
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode);
-  const data = JSON.parse(body);
-  callback(null, data[0].description);
-});
-}
+    if (body === '[]') {
+      callback('ERROR: Breed not found', null);
+      return;
+    }
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    const data = JSON.parse(body);
+    callback(null, data[0].description);
+  });
+};
 
 module.exports = { fetchBreedDescription };
 
