f291fbeb9c82800cc383ec205166ed04f881c15d
ShannaJSmith <shanna_smith987@hotmail.com>
2021-09-23 19:26:20 +0000

    eslint

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const cmlArgs = process.argv.slice(2);
-const url = 'https://api.thecatapi.com/v1/breeds/search?q='
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
 const breed = cmlArgs[0];
 
 request(`${url}${breed}`, (error, response, body) => {
@@ -12,8 +12,8 @@ request(`${url}${breed}`, (error, response, body) => {
     console.log('ERROR');
     return;
   }
-  console.log('error:', error); 
-  console.log('statusCode:', response && response.statusCode); 
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
   const data = JSON.parse(body);
   console.log('body:', data[0].description);
 });
